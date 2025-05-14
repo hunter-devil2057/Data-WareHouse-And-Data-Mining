db5c3a1416221876460734333736ce258a0d82f0
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2025-05-14 06:01:12 +0545

    Standard Scaler Normailzation

---
+++ Practical/Lab-1/Standard-Scaler.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+class StandardNorm:
+  def scale(self,d):
+    for i in d.columns:
+      mean=d[i].mean()
+      sd=d[i].std()      
+      d[i]=(d[i]-mean)/sd     
+    return d
+data=pd.DataFrame([[45000,42],[32000,26],[58000,48],[37000,32]],columns=['Salary','Age'])
+print("Original Data")
+print(data)
+s=StandardNorm()
+df=s.scale(data)
+print("\nScaled Data")
+print(df)
