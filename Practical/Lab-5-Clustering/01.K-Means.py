1b0dc70de6fa371fe90ae93289aa22107ed99a6d
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2025-05-14 06:05:02 +0545

    K-Means Algorithm

---
+++ Practical/Lab-5-Clustering/01.K-Means.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+data=100*np.random.rand(1000,2)
+print(*data)
+km=KMeans(n_clusters=3,init='random')
+km.fit(data)
+centers = km.cluster_centers_
+labels = km.labels_
+print("Cluster Centers:",*centers)
+print("Cluster Labels:",*labels)
+
+colors = ["r","g","b"]
+markers=["+","x","*"]
+for i in range(len(data)):
+    plt.plot(data[i][0], data[i][1], color=colors[labels[i]], marker=markers[labels[i]])
+plt.scatter(centers[:, 0],centers[:, 1], marker = "o", s=50, linewidths = 5)
+plt.show()
